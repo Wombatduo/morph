565307281b34f60e36800afa7571489b120ad890
Wombatduo <v-laburenko@yandex.ru>
2018-05-05 21:15:48 +0300

    Added New empty test for verb morpher

---
+++ src/duo/wombat/VerbMorpherTest.java
@@ -0,0 +1,21 @@
+package duo.wombat;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VerbMorpherTest {
+
+    @Before
+    public void setup(){
+
+    }
+
+    @Test
+    public void checkIrregularVerbsPastForm(){
+
+    }
+
+}
